4ba375fac347629915e429a8570d40e3e9c8f905
unknown <whitehead@PC801.wehi.edu.au>
2015-10-21 16:53:16 +1100

    Adding comments, plots etc

--- HerdImmunity.py
+++ HerdImmunity.py
@@ -11,22 +11,41 @@ from matplotlib import pyplot as plt
     
 
 def main():
+    
     tStore = []
     healthSurvStore = []
     freeSurvStore = []
-    valRange = np.arange(.5,1,.025)    
+    valRange = np.arange(.0 ,1,.01)    
     
     for immunity in valRange:
-        t,h,f = repeatSimAndAverage(5,immunity)  
+        t,h,f = repeatSimAndAverage(100,immunity)  
         tStore.append(t)
         healthSurvStore.append(h)
         freeSurvStore.append(f)
+    
+    with plt.xkcd():
+        fig,ax = plt.subplots(1,2,figsize=(10,4))
+        #fig.tight_layout()
+        ax[0].scatter(valRange,healthSurvStore,color='r')
+        ax[0].set_title('% Susceptible Survival')
+        ax[0].set_xlabel('% immunised')
+        ax[0].set_xlim(-.10,1)
+        ax[0].set_ylim(-10,100)
+        ax[0].set_ylabel('% survival')
         
+    
         
-    plt.plot(valRange,healthSurvStore)
+        ax[1].scatter(valRange,tStore)
+        ax[1].set_title('Virus perpetuated for n rounds')
+        ax[1].set_xlabel('% immunised')
+        ax[1].set_xlim(0,1)        
+        ax[1].set_ylabel('Time')
     
 def repeatSimAndAverage(repeats,valueToAlter):
-    repeats = 10
+    ''' Runs a loop of the simulation 
+        Pass a value to allow changing a simulation parameter
+        Parameter to change set in runSim()'''
+        
     tStore = []
     healthSurvStore = []
     freeSurvStore = []
@@ -39,13 +58,17 @@ def repeatSimAndAverage(repeats,valueToAlter):
     
 
 def runSim(doBatch,value):
+    '''Sets up and runs the simulation
+        value passed by repeatSimAndAverage()
+        Will change depending on setupAndGetPeople()'''   
+    
     tic = time.time()
-    imsize = 64
+    imsize = 32
     people,im = setupAndGetPeople(imsize,
-                                  fraction = 4, #density of people
+                                  fraction = 2, #density of people
                                   n_infect = 1, #number of initial infections
                                   fraction_immune = value,
-                                  fraction_freeloader = .0,   #fraction of immune who choose note to
+                                  fraction_freeloader = 0.0,   #fraction of immune who choose note to
                                   deathRate = 1.0, #chance of death from infection
                                   batch = doBatch)                          
     if not doBatch:
@@ -57,14 +80,7 @@ def runSim(doBatch,value):
                                                          maxTime = 1000,
                                                          batch = doBatch
                                                          )
-
-    
-    #draw again at the end       
-    #image = makeImage(people,imsize)
-    #im.set_data(image)
-    #plt.draw()
-   
-        
+         
     elapsed = time.time() - tic
     print '%f seconds to run' % elapsed
     return t,healthySurvivePercent,freeSurvivePercent
@@ -76,9 +92,12 @@ def runLoop(people,
             liveUpdate = True,
             mortalityGraph = True,
             batch = False):
+    '''Loops the simulation in time 
+        Needs to be passed a list of people, the image reference (im)
+        and the image size'''
 
     if batch:
-        '''batch over-rides other options'''
+        #batch over-rides other options
         liveUpdate = False
         mortalityGraph = False 
                 
@@ -113,7 +132,6 @@ def runLoop(people,
                     
         
         mort.append((t,n_dead,len(infectedPeople)))
-                        
                 
         if liveUpdate: 
             plt.title('Infected people: %i\nPeople killed %i' % 
@@ -141,11 +159,6 @@ def runLoop(people,
         freeloadSurvivePercent = (100.*(float(freeloadSurvivors) / freeloadStart))  
     except:
         freeloadSurvivePercent = -1.0
-        
-        
-   # print 'Virus survived for %i rounds' % t
-  #  print '%.2f%% of un-vaccinateable survived' % healthySurvivePercent
-   # print '%.2f%% of freeloaders survived' % freeloadSurvivePercent
     
     return t, healthySurvivePercent, freeloadSurvivePercent
 
@@ -156,6 +169,8 @@ def setupAndGetPeople(imsize = 128,
                       fraction_freeloader = .1,
                       deathRate = .1,
                       batch = False):
+    '''Sets up the "population" for the simulation, 
+    returns as a list of people/pixels'''
                           
     plt.close('all')
     numberOfPeople = int((imsize*imsize)/fraction)
@@ -166,14 +181,16 @@ def setupAndGetPeople(imsize = 128,
     n_immune = n_immune - n_free
     
     n_healthy = n_healthy - n_immune - n_free
-    status = ('\nStarting Conditions\nTotal people: %i\t'
-              'Healthy people: %d\n'
-              'Immunised: %d\t'
-              'Freeloaders: %d' % (numberOfPeople,
-                                  float(n_healthy)/1,
-                                  float(n_immune)/1,
-                                  float(n_free)/1))
-    print status
+
+    if not batch:    
+        status = ('\nStarting Conditions\nTotal people: %i\t'
+                  'Healthy people: %d\n'
+                  'Immunised: %d\t'
+                  'Freeloaders: %d' % (numberOfPeople,
+                                      float(n_healthy)/1,
+                                      float(n_immune)/1,
+                                      float(n_free)/1))
+        print status
     
      
     people = initPeople(imsize,n_healthy,n_infect,n_immune,n_free,deathRate)
@@ -197,7 +214,7 @@ def setupAndGetPeople(imsize = 128,
                       
 
 class pixel():
-    def __init__(self,imageSize,position,status='healthy',deathRate=.1):
+    def __init__(self,imageSize,position,status='healthy',deathRate=0.1):
         '''pixel has an initial position and is healthy by default
         Options for status are  'healthy'
                                 'infected'
@@ -218,7 +235,7 @@ class pixel():
                         'dead':[.5,.1,.1]}
         
         self.value = self.statusColor[self.status]
-        self.deathrate = deathRate
+        self.deathRate = deathRate
         
         
     def move(self,infectedPeoplePositions):
@@ -239,7 +256,8 @@ class pixel():
             
                 if self.lengthOfInfection >= infectionLength:              
                     coinflip = np.random.rand(1)
-                    if coinflip[0] < 0.95:                                 
+                                       
+                    if coinflip[0] < self.deathRate:                                 
                         self.status = 'dead'                           
                     else:                    
                         self.status = 'immune'
@@ -283,20 +301,19 @@ def initPeople(imsize,numberOfPeople,n_infect,n_immune,n_freeloaders,deathRate):
     
     for i in range(0,numberOfPeople):
         newPos = [np.random.randint(0,imsize),np.random.randint(0,imsize)]        
-        people.append(pixel(imsize,newPos))
-    
+        people.append(pixel(imsize,newPos,deathRate=deathRate))
+        
     for i in range(0,n_infect):
         newPos = [np.random.randint(0,imsize),np.random.randint(0,imsize)]        
-        people.append(pixel(imsize,newPos,status='infected'))
+        people.append(pixel(imsize,newPos,status='infected',deathRate=deathRate))
     
     for i in range(0,n_immune):
         newPos = [np.random.randint(0,imsize),np.random.randint(0,imsize)]        
-        people.append(pixel(imsize,newPos,status='immune'))
+        people.append(pixel(imsize,newPos,status='immune',deathRate=deathRate))
         
     for i in range(0,n_freeloaders):
         newPos = [np.random.randint(0,imsize),np.random.randint(0,imsize)]        
-        people.append(pixel(imsize,newPos,status='freeloader'))
-    
+        people.append(pixel(imsize,newPos,status='freeloader',deathRate=deathRate))
     return people
     
 
